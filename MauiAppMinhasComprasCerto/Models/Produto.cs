4670ba86271657e50070d30599c7d776dffec33b
AlineCosme <linoca2008@hotmail.com>
2025-03-20 21:46:57 -0300

    Finalização da atividade da Agenda 5

--- MauiAppMinhasComprasCerto/Models/Produto.cs
+++ MauiAppMinhasComprasCerto/Models/Produto.cs
@@ -3,9 +3,22 @@ namespace MauiAppMinhasComprasCerto.Models
 {
    public class Produto
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+                 }
 
         public double Quantidade { get; set; }
 
